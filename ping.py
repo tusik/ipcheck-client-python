8e087e2e20621e85994a6454142e13af3565bfae
Image <onenookone@gmail.com>
2018-12-10 21:08:26 +0800

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,7 +3,8 @@ import socket,json,time,binascii,os
 import uuid,configparser,requests
 from ipaddress import *
 import pingparsing
-
+from threading import Timer, Thread
+from time import sleep, time
 def checkSum():
     blocksize = 1024 * 64 
     f = open(__file__,"rb") 
@@ -23,7 +24,7 @@ __SERVER_NAME__ = conf.get("server","name")
 __SERVER_UUID__ = conf.get("server","uuid")
 __SERVER_HOST__ = conf.get("server","host")
 __SERVER_DEBUG__ = False if conf.getint("server","debug")==0 else True
-__version__="0.1.0"
+__version__="0.1.1"
 if len(__SERVER_UUID__)<10:
     conf.set("server","uuid",str(uuid.uuid1()))
     with open(__CONFILE__,"w+",encoding='utf8') as f:
@@ -31,6 +32,28 @@ if len(__SERVER_UUID__)<10:
 __SERVER_CHECKSUM__ = checkSum()+binascii.crc32(__SERVER_UUID__.encode())
 
 app = Flask(__name__)
+class ReportScheduler(object):
+    def __init__(self, sleep_time, function):
+        self.sleep_time = sleep_time
+        self.function = function
+        self._t = None
+
+    def start(self):
+        if self._t is None:
+            self._t = Timer(self.sleep_time, self._run)
+            self._t.start()
+        else:
+            raise Exception("this timer is already running")
+
+    def _run(self):
+        self.function()
+        self._t = Timer(self.sleep_time, self._run)
+        self._t.start()
+
+    def stop(self):
+        if self._t is not None:
+            self._t.cancel()
+            self._t = None
 
 # IP Class for save ping result
 class IP:
@@ -77,7 +100,7 @@ def requestCheckSum(domain,ts):
 def startPing(ip):
     ping_parser = pingparsing.PingParsing()
     transmitter = pingparsing.PingTransmitter()
-    transmitter.count = 5
+    transmitter.count = 1
     transmitter.destination_host = ip
     ping_res = transmitter.ping()
     if ping_res.returncode == 0:
@@ -130,6 +153,11 @@ def getPing(domain,cs,ts):
 def checkClient():
     res = requests.post("https://if.uy/client/"+__version__+"/"+str(checkSum())).text
     return res
+
+def sendAlive():
+    if requests.get("https://if.uy/client/alive/uuid="+__SERVER_UUID__).status_code != 200:
+        print("Get error from server")
+
 if __name__ == '__main__':
     print(":::::::::::::::::::::System Info::::::::::::::::::::::")
     print("My Client version is: ",__version__)
@@ -139,6 +167,10 @@ if __name__ == '__main__':
         print(":::::::::::::::::::::ERROR::::::::::::::::::::::")
         os._exit(1)
     print(":::::::::::::::::::::System Info::::::::::::::::::::::")
+
+    scheduler = ReportScheduler(0.25*60*60, sendAlive)
+    scheduler.start()
+
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=__SERVER_DEBUG__,port=__SERVER_PORT__,host=__SERVER_HOST__)
     
