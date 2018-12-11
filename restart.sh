8fe5f739139dc7b01cda7ae7b38063a9de337317
tusik <onenookone@gmail.com>
2018-12-11 16:49:06 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 kill -9 $(ps -ef | grep 'python3 ping.py' | grep -v grep | awk '{print $2}')
+git fetch --all
+git reset --hard origin/master 
 git pull
 nohup python3 ping.py > nohup2.out 2>&1&
